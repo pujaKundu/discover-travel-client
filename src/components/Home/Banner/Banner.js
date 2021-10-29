c7a8bdf9b48f3475fbeccebee7ee229749cbf857
pujaKundu <iampujakundu@gmail.com>
2021-10-29 13:35:09 +0600

    added banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import banner from "../../../images/Banner/banner-2.jpg";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>This is banner</h1>
-    </div>
+    <Container
+      className=" d-flex flex-column aign-items-center justify-content-center"
+      fluid
+      style={{
+        backgroundImage: `url(${banner})`,
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <h1 className="display-2 banner-text">
+        Discover the most amazing places
+      </h1>
+      <h3 className="text-white sub-text">
+        Best places for your next travel are here check them out
+      </h3>
+    </Container>
   );
 };
 
--- src/components/Home/Homepage/Homepage.js
+++ src/components/Home/Homepage/Homepage.js
@@ -1,11 +1,8 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Homepage = () => {
-  return (
-    <div>
-      <h1>This is homepage</h1>
-    </div>
-  );
+  return <Banner></Banner>;
 };
 
 export default Homepage;
